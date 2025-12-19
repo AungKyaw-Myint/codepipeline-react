6b2a057d695000dddd7f24f09140860e34f72770
AungKyawMyint <52407468+AungKyaw-Myint@users.noreply.github.com>
2025-12-19 15:17:06 -0600

    test automation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World1 Testing Automation</h1>
+      <h1>Hello World1 Testing Automation 2</h1>
     </div>
   );
 }
