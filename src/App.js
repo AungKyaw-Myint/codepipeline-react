4b338621d6c35b50419a7b0865e5abed7e392c0a
AungKyawMyint <52407468+AungKyaw-Myint@users.noreply.github.com>
2025-12-19 16:06:57 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World1 Testing Automation 4</h1>
+      <h1>Hello World1 Testing Automation 2</h1>
     </div>
   );
 }
